84fe6eb51091a8256cbe891ba1a41736919feeea
Nurullah GUNGOR <ngngrx1@gmail.com>
2024-03-15 05:24:06 +0300

    forgot password sayfasındaki hatalar düzeltildi, özellikle app/mail/AdminSendForgotPasswordMail içerisindeki hatanın sebebi __construct metotuna değişkenleri eklemeyi unutmuşum ve bunları ekledikten sonra hata düzeldi.

--- app/Http/Controllers/Admin/AdminAuthenticatorController.php
+++ app/Http/Controllers/Admin/AdminAuthenticatorController.php
@@ -3,24 +3,31 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Admin;
 use Illuminate\Http\Request;
-use App\Http\Requests\Auth\HandleLoginRequest;
-use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Password;
+use App\Mail\AdminSendForgotPasswordMail;
+use App\Http\Requests\Auth\HandleLoginRequest;
+use App\Http\Requests\Auth\HandleForgotPassword;
+
 
 class AdminAuthenticatorController extends Controller
 {
+    /* Login */
     public function login(){
         return view('admin.auth.login');
     }
-
+    /* Handle Login */
     public function handleLogin(HandleLoginRequest $request){
         $request->authenticate();
 
         return redirect() -> route('admin.dashboard');
     }
 
-
+    /* logout */
     public function logout(Request $request): RedirectResponse{
         Auth::guard('admin')->logout();
 
@@ -30,4 +37,27 @@ class AdminAuthenticatorController extends Controller
 
         return redirect()->route('admin.login');
     }
+
+
+    public function forgotPassword(){
+        return view('admin.auth.forgot-password');
+    }
+
+    public function handleForgotPassword(HandleForgotPassword $request){
+        $token = \Str::random(64);
+
+        $admin = Admin::where('email', $request->email)->first();
+        $admin->remember_token = $token;
+        $admin->save();
+        Mail::to($request->email)->send(new AdminSendForgotPasswordMail($token, $request->email));
+
+        return redirect()->back()->with('success', __('admin.A mail has been sent to your email address please check!'));
+
+    }
+
+    public function resetPassword($token)
+    {
+        return view('admin.auth.reset-password', compact('token'));
+    }
+
 }
